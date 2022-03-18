0ef7d607ef28e50610fb67c4949dd0a7df086755
Dhanuja05 <dhanu.doodleblue@gmail.com>
2022-03-18 18:09:17 +0530

    'DJJ;

---
+++ Day8Proj/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Reg from './Reg.js'
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <Reg/>
+      </div>
+    )
+  }
+}
+
+export default App
