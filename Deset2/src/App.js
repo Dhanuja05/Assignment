b4faf2b1b8614b3429f7518a9f5fff712846b783
Dhanuja05 <dhanu.doodleblue@gmail.com>
2022-03-18 23:03:21 +0530

    'des'

---
+++ Deset2/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Dest1 from './Dest1.js'
+export class App extends Component {
+  render() {
+    return (
+      <div>
+      <Dest1/>
+      </div>
+    )
+  }
+}
+
+export default App
