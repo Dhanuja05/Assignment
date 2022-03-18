12e6ef726a2bd62642c87c728df338f79dc02baa
Dhanuja05 <dhanu.doodleblue@gmail.com>
2022-03-18 23:06:24 +0530

    'vali'

---
+++ valid/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Valid from './Valid.js'
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <Valid/>
+      </div>
+    )
+  }
+}
+
+export default App
